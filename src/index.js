f1ad413b62a37bdcd7c0be0fc076541024b91d55
Melodyn <samelodyn@gmail.com>
2020-05-23 22:37:31 +0300

    step 2

--- src/index.js
+++ src/index.js
@@ -3,5 +3,10 @@ import program from 'commander';
 export default () => {
   program.version('0.0.1')
     .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format [type]', 'output format')
+    .arguments('<filepath1> <filepath2>')
+    .action((filepath1, filepath2) => {
+      console.log(`format: ${program.format}, f1: ${filepath2}, f2: ${filepath2}`);
+    })
     .parse(process.argv);
 };
