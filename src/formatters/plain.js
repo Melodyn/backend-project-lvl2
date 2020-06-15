ba56847e82706d8233ac10fc300fc2009f0911bb
Melodyn <samelodyn@gmail.com>
2020-06-15 21:32:55 +0300

    refactoring

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -2,32 +2,37 @@ import _ from 'lodash';
 import types from '../types.js';
 
 const processValue = (value) => (_.isObject(value) ? '[complex value]' : value);
+const processKey = (prefix, key) => (_.isEmpty(prefix) ? key : `${prefix}.${key}`);
 
-const formatPlain = (diffTree) => diffTree
-  .flatMap((node) => {
-    const {
-      type, key, previousValue, currentValue, children,
-    } = node;
-    const processedPrevValue = processValue(previousValue);
-    const processedCurrValue = processValue(currentValue);
+const formatPlain = (diffTree) => {
+  const iter = (tree, parentName) => tree
+    .flatMap((node) => {
+      const {
+        type, key, previousValue, currentValue, children,
+      } = node;
+      const processedPrevValue = processValue(previousValue);
+      const processedCurrValue = processValue(currentValue);
+      const processedKey = processKey(parentName, key);
 
-    switch (type) {
-      case types.deleted:
-        return `Property '${key}' was deleted`;
-      case types.added:
-        return `Property '${key}' was added with value: ${processedCurrValue}`;
-      case types.changed:
-        return `Property '${key}' was changed from ${processedPrevValue} to ${processedCurrValue}`;
-      case types.nested:
-        return formatPlain(children.map(
-          ({ key: childKey, ...other }) => ({ key: `${key}.${childKey}`, ...other }),
-        ));
-      case types.consist:
-        return null;
-      default:
-        throw new Error(`Unexpected type ${type}`);
-    }
-  })
-  .filter(_.identity);
+      switch (type) {
+        case types.deleted:
+          return `Property '${processedKey}' was deleted`;
+        case types.added:
+          return `Property '${processedKey}' was added with value: ${processedCurrValue}`;
+        case types.changed:
+          return `Property '${processedKey}' was changed from ${processedPrevValue} to ${processedCurrValue}`;
+        case types.nested:
+          return iter(children, processedKey);
+        case types.consist:
+          return null;
+        default:
+          throw new Error(`Unexpected type ${type}`);
+      }
+    });
+
+  const strings = iter(diffTree, '');
+
+  return strings.filter(_.identity);
+};
 
 export default (data) => formatPlain(data).join('\n');
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -3,62 +3,77 @@ import types from '../types.js';
 
 const labels = { deleted: '-', added: '+', consist: ' ' };
 
-// like JSON.stringify(object, null, 2), but without quotes
-const stringify = (sourceObject, replacer, startLineOffset = 0, endLineOffset = 0) => {
-  const openSymbol = '{';
-  const closeSymbol = '}';
-  const indentSymbol = replacer || ' ';
+// // like JSON.stringify(object, null, 2), but without quotes
+// const stringify = (sourceObject, replacer, startLineOffset = 0, endLineOffset = 0) => {
+//   const openSymbol = '{';
+//   const closeSymbol = '}';
+//   const indentSymbol = replacer || ' ';
+//
+//   const iter = (currentObject, openLineSymbol, closeLineSymbol, countOfIndents) => {
+//     const lines = _.entries(currentObject).flatMap(([key, value]) => {
+//       const openIndent = indentSymbol.repeat(countOfIndents);
+//       const closeIndent = indentSymbol.repeat(countOfIndents + endLineOffset);
+//       const preparedKey = openIndent + key;
+//
+//       if (_.isObject(value)) {
+//         const openLine = `${preparedKey}: ${openSymbol}`;
+//         const closeLine = closeIndent + closeSymbol;
+//         const deepCountOfIndents = countOfIndents + startLineOffset + endLineOffset;
+//
+//         return iter(value, openLine, closeLine, deepCountOfIndents);
+//       }
+//
+//       return `${preparedKey}: ${value}`;
+//     });
+//
+//     return [openLineSymbol, ...lines, closeLineSymbol];
+//   };
+//
+//   const lines = iter(sourceObject, openSymbol, closeSymbol, startLineOffset);
+//
+//   return lines.join('\n');
+// };
 
-  const iter = (currentObject, openLineSymbol, closeLineSymbol, countOfIndents) => {
-    const lines = _.entries(currentObject).flatMap(([key, value]) => {
-      const openIndent = indentSymbol.repeat(countOfIndents);
-      const closeIndent = indentSymbol.repeat(countOfIndents + endLineOffset);
-      const preparedKey = openIndent + key;
+const indentSymbol = '  ';
+const openSymbol = '{';
+const closeSymbol = '}';
 
-      if (_.isObject(value)) {
-        const openLine = `${preparedKey}: ${openSymbol}`;
-        const closeLine = closeIndent + closeSymbol;
-        const deepCountOfIndents = countOfIndents + startLineOffset + endLineOffset;
-
-        return iter(value, openLine, closeLine, deepCountOfIndents);
-      }
-
-      return `${preparedKey}: ${value}`;
-    });
-
-    return [openLineSymbol, ...lines, closeLineSymbol];
-  };
-
-  const lines = iter(sourceObject, openSymbol, closeSymbol, startLineOffset);
-
-  return lines.join('\n');
+const stringify = (data, depth) => {
+  if (!_.isObject(data)) {
+    return data;
+  }
+  const indent = indentSymbol.repeat(depth);
 };
 
-const formatStylish = (diffTree) => diffTree
-  .reduce((acc, node) => {
+const formatStylish = (diffTree) => {
+  const iter = (tree, depth) => tree.flatMap((node) => {
     const {
       type, key, previousValue, currentValue, children,
     } = node;
+    const indent = indentSymbol.repeat(depth);
+    const processedPrevValue = stringify(previousValue);
+    const processedCurrValue = stringify(currentValue);
 
     switch (type) {
       case types.deleted:
-        return { ...acc, [`${labels.deleted} ${key}`]: previousValue };
+        return `${indent}${labels.deleted} ${key}: ${processedPrevValue}`;
       case types.added:
-        return { ...acc, [`${labels.added} ${key}`]: currentValue };
+        return `${indent}${labels.added} ${key}: ${processedCurrValue}`;
       case types.changed:
-        return {
-          ...acc,
-          [`${labels.deleted} ${key}`]: previousValue,
-          [`${labels.added} ${key}`]: currentValue,
-        };
+        return [
+          `${indent}${labels.added} ${key}: ${processedCurrValue}`,
+          `${indent}${labels.deleted} ${key}: ${processedPrevValue}`,
+        ].join('\n');
       case types.nested:
-        return { ...acc, [`${labels.consist} ${key}`]: formatStylish(children) };
+        return `${indent}${labels.consist} ${key}: ${iter(children, depth + 1)}`;
       case types.consist:
-        return { ...acc, [`${labels.consist} ${key}`]: currentValue };
+        return `${indent}${labels.consist} ${key}: ${processedCurrValue}`;
       default:
         throw new Error(`Unexpected type ${type}`);
     }
-  },
-  {});
+  });
 
-export default (data) => stringify(formatStylish(data), null, 2, 2);
+  return iter(diffTree, 1);
+};
+
+export default (data) => formatStylish(data).join('\n');
