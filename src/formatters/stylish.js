b639c38fe2bc40b65d43e36839c2cd6ab10b2b03
Melodyn <samelodyn@gmail.com>
2020-06-16 17:51:25 +0300

    refactor stylish

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -2,78 +2,63 @@ import _ from 'lodash';
 import types from '../types.js';
 
 const labels = { deleted: '-', added: '+', consist: ' ' };
-
-// // like JSON.stringify(object, null, 2), but without quotes
-// const stringify = (sourceObject, replacer, startLineOffset = 0, endLineOffset = 0) => {
-//   const openSymbol = '{';
-//   const closeSymbol = '}';
-//   const indentSymbol = replacer || ' ';
-//
-//   const iter = (currentObject, openLineSymbol, closeLineSymbol, countOfIndents) => {
-//     const lines = _.entries(currentObject).flatMap(([key, value]) => {
-//       const openIndent = indentSymbol.repeat(countOfIndents);
-//       const closeIndent = indentSymbol.repeat(countOfIndents + endLineOffset);
-//       const preparedKey = openIndent + key;
-//
-//       if (_.isObject(value)) {
-//         const openLine = `${preparedKey}: ${openSymbol}`;
-//         const closeLine = closeIndent + closeSymbol;
-//         const deepCountOfIndents = countOfIndents + startLineOffset + endLineOffset;
-//
-//         return iter(value, openLine, closeLine, deepCountOfIndents);
-//       }
-//
-//       return `${preparedKey}: ${value}`;
-//     });
-//
-//     return [openLineSymbol, ...lines, closeLineSymbol];
-//   };
-//
-//   const lines = iter(sourceObject, openSymbol, closeSymbol, startLineOffset);
-//
-//   return lines.join('\n');
-// };
-
 const indentSymbol = '  ';
+const deepKeyOffset = 2;
 const openSymbol = '{';
 const closeSymbol = '}';
 
-const stringify = (data, depth) => {
+const addPrefix = (symbol, indent, label = ' ') => `${indent}${label} ${symbol}`;
+
+const stringify = (data, indent) => {
   if (!_.isObject(data)) {
     return data;
   }
-  const indent = indentSymbol.repeat(depth);
+
+  const nestedIndent = addPrefix(indentSymbol, indent);
+  const strings = [
+    openSymbol,
+    ...(_.entries(data).flatMap(
+      ([key, value]) => `${addPrefix(key, nestedIndent)}: ${stringify(value, nestedIndent)}`,
+    )),
+    addPrefix(closeSymbol, indent),
+  ];
+
+  return strings.join('\n');
 };
 
 const formatStylish = (diffTree) => {
-  const iter = (tree, depth) => tree.flatMap((node) => {
+  const iter = (tree, currentKeysOffset) => tree.flatMap((node) => {
+    const indent = indentSymbol.repeat(currentKeysOffset);
     const {
       type, key, previousValue, currentValue, children,
     } = node;
-    const indent = indentSymbol.repeat(depth);
-    const processedPrevValue = stringify(previousValue);
-    const processedCurrValue = stringify(currentValue);
 
     switch (type) {
       case types.deleted:
-        return `${indent}${labels.deleted} ${key}: ${processedPrevValue}`;
+        return [`${addPrefix(key, indent, labels.deleted)}: ${stringify(previousValue, indent)}`];
       case types.added:
-        return `${indent}${labels.added} ${key}: ${processedCurrValue}`;
+        return [`${addPrefix(key, indent, labels.added)}: ${stringify(currentValue, indent)}`];
       case types.changed:
         return [
-          `${indent}${labels.added} ${key}: ${processedCurrValue}`,
-          `${indent}${labels.deleted} ${key}: ${processedPrevValue}`,
-        ].join('\n');
+          `${addPrefix(key, indent, labels.deleted)}: ${stringify(previousValue, indent)}`,
+          `${addPrefix(key, indent, labels.added)}: ${stringify(currentValue, indent)}`,
+        ];
       case types.nested:
-        return `${indent}${labels.consist} ${key}: ${iter(children, depth + 1)}`;
+        return [
+          `${addPrefix(key, indent, labels.consist)}: ${openSymbol}`,
+          ...(iter(children, currentKeysOffset + deepKeyOffset)),
+          `${addPrefix(closeSymbol, indent)}`,
+        ];
       case types.consist:
-        return `${indent}${labels.consist} ${key}: ${processedCurrValue}`;
+        return [`${addPrefix(key, indent, labels.consist)}: ${stringify(currentValue, indent)}`];
       default:
         throw new Error(`Unexpected type ${type}`);
     }
   });
 
-  return iter(diffTree, 1);
+  const strings = iter(diffTree, 1);
+
+  return [openSymbol, ...strings, closeSymbol];
 };
 
 export default (data) => formatStylish(data).join('\n');
