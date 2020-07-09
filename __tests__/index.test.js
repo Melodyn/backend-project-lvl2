a804686e26a615a67def9e28350e8e1952f4dbcd
Melodyn <samelodyn@gmail.com>
2020-07-09 10:36:37 +0300

    remove toolkit

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,11 +1,19 @@
-import { buildPath, readFile } from '../src/filesToolkit.js';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import gendiff from '../index.js';
+
+/* eslint-disable no-underscore-dangle */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) => path.resolve(__dirname, '..', '__fixtures__', filename);
+const readFile = (pathToFile) => fs.readFileSync(getFixturePath(pathToFile), 'utf-8');
 
 const outputFormats = ['stylish', 'plain', 'json'];
 const extensions = ['yml', 'ini', 'json'];
 const expectedOutputByFormat = {};
 
-const getFixturePath = (filename) => buildPath('__fixtures__', filename);
 const combinations = extensions.flatMap(
   (extension) => outputFormats.map((format) => [extension, format]),
 );
@@ -27,5 +35,3 @@ test.each(combinations)('diff .%s-file formatted as %s', (extension, outputForma
   const expected = expectedOutputByFormat[outputFormat];
   expect(actual).toEqual(expected);
 });
-
-test('relative & full path', () => {});
--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-import { readFile, getExtension } from './src/filesToolkit.js';
+import fs from 'fs';
+import path from 'path';
 import toDiffTree from './src/toDiffTree.js';
 import parse from './src/parsers.js';
 import format from './src/formatters/index.js';
+
+export const getExtension = (filepath) => path.extname(filepath).substring(1);
+export const readFile = (pathToFile) => fs.readFileSync(path.resolve(pathToFile), 'utf8');
 
 export default (filepathBefore, filepathAfter, outputFormat) => {
   const dataBefore = parse(readFile(filepathBefore), getExtension(filepathBefore));
