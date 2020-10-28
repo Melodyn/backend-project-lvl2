db0a39f1085d48ffd3a9c691b5a658af42d7dc13
Melodyn <samelodyn@gmail.com>
2020-10-28 13:40:19 +0300

    refactoring

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,29 +1,29 @@
 import _ from 'lodash';
 import types from '../types.js';
 
+const keyOffset = 4;
+const prefixOffset = 2;
+const indentSymbol = ' ';
+const openSymbol = '{';
+const closeSymbol = '}';
 const labels = {
   deleted: '-',
   added: '+',
   unchanged: ' ',
   nested: ' ',
 };
-const keyOffset = 4;
-const prefixOffset = 2;
-const indentSymbol = ' ';
-const openSymbol = '{';
-const closeSymbol = '}';
 
 const addPrefix = (key, type, indent) => `${indent}${labels[type]} ${key}`;
 const stringify = (value, depth) => {
   if (!_.isObject(value)) {
-    return `${value}`;
+    return value;
   }
 
   const indentSize = depth * keyOffset;
-  const indent = indentSymbol.repeat(indentSize);
+  const keyIndent = indentSymbol.repeat(indentSize);
   const bracketIndent = indentSymbol.repeat(indentSize - keyOffset);
   const lines = Object.entries(value)
-    .flatMap(([key, val]) => `${indent}${key}: ${stringify(val, depth + 1)}`);
+    .flatMap(([key, val]) => `${keyIndent}${key}: ${stringify(val, depth + 1)}`);
 
   return [
     openSymbol,
@@ -41,26 +41,26 @@ const formatStylish = (diffTree) => {
     const indentSize = depth * keyOffset;
     const keyIndent = indentSymbol.repeat(indentSize - prefixOffset);
     const bracketIndent = indentSymbol.repeat(indentSize);
+    const toStr = (value) => stringify(value, depth + 1);
 
     switch (type) {
       case types.deleted:
-        return `${addPrefix(key, type, keyIndent)}: ${stringify(value1, depth + 1)}`;
+        return `${addPrefix(key, type, keyIndent)}: ${toStr(value1)}`;
       case types.added:
-        return `${addPrefix(key, type, keyIndent)}: ${stringify(value2, depth + 1)}`;
+        return `${addPrefix(key, type, keyIndent)}: ${toStr(value2)}`;
       case types.changed:
         return [
-          `${addPrefix(key, types.deleted, keyIndent)}: ${stringify(value1, depth + 1)}`,
-          `${addPrefix(key, types.added, keyIndent)}: ${stringify(value2, depth + 1)}`,
+          `${addPrefix(key, types.deleted, keyIndent)}: ${toStr(value1)}`,
+          `${addPrefix(key, types.added, keyIndent)}: ${toStr(value2)}`,
         ];
       case types.unchanged:
-        return `${addPrefix(key, type, keyIndent)}: ${stringify(value2, depth + 1)}`;
-      case types.nested: {
+        return `${addPrefix(key, type, keyIndent)}: ${toStr(value2)}`;
+      case types.nested:
         return [
           `${addPrefix(key, type, keyIndent)}: ${openSymbol}`,
           ...iter(children, depth + 1),
           `${bracketIndent}${closeSymbol}`,
         ];
-      }
       default:
         throw new Error(`Unexpected node type ${type}`);
     }
