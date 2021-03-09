bc743c3df051b493d56a802b2eef8dbb16837245
Melodyn <samelodyn@gmail.com>
2021-03-09 03:20:18 +0300

    fix commander options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,7 +7,7 @@ program.version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => {
-    console.log(gendiff(filepath1, filepath2, program.format));
+  .action((filepath1, filepath2, options) => {
+    console.log(gendiff(filepath1, filepath2, options.format));
   })
   .parse(process.argv);
